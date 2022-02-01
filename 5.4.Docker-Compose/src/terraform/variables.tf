e3c463bd64f4caaa5cbc9b18336383354de08cba
Vitaly Kaydalov <vitalykay@gmail.com>
2022-02-01 11:16:19 +0300

    Add 5.4.Docker-Compose

---
+++ 5.4.Docker-Compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gugppm6d6jifng28t4"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gf6kkr9lt4lofiqh08"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8fmkujbopb1vpq32uf"
+}
